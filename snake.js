96085b050112d07a4cb30372f1ca58d9f0463f51
datasaysalot <61099792+datasaysalot@users.noreply.github.com>
2020-03-30 01:03:54 -0500

    Add files via upload

---
+++ snake.js
@@ -0,0 +1,16 @@
+/* import html2canvas from 'html2canvas';
+
+function getScreenshot () {
+	html2canvas(document.body, {
+		onrendered: function(canvas) {
+			$('#pic').html("");
+			$('#pic').append(canvas);
+		}
+	});
+
+} */
+function myFunction(){
+	
+document.getElementById("1").innerHTML = "What?";
+
+}
